af3e23e420536d6883362e3b4ba37c9ae0f9cc17
thazaww <thazaww@gmail.com>
2025-09-16 16:23:19 +0700

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -1,3 +1,13 @@
 from aiogram import Dispatcher
+from pyrogram import Client
+from config import API_ID, API_HASH, BOT_TOKEN
 
 dp = Dispatcher()
+
+# Pyrogram client (bot account)
+SmartPyro = Client(
+    "bot",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN
+)
